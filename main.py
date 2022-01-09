739d01ffef2599e14504d6a1c37d8e27c6c79f52
Hans Yu <hans.yu@outlook.de>
2022-01-09 23:15:29 +0100

    First draft of State.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 import enum
+import typing
 
 class Action(enum.Enum):
     NORTH = enum.auto()
     SOUTH = enum.auto()
     EAST = enum.auto()
     WEST = enum.auto()
+
+class State(typing.NamedTuple):
+    row: int
+    col: int
 
 if __name__ == "__main__":
     for action_it in Action:
